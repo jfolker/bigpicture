de33eb490797c15ee2014d16e649e9437dd6dbc6
Jory Folker <38365438+jfolker@users.noreply.github.com>
2023-05-25 16:11:50 -0500

    Make bparchived.cpp force shutdown after the second ctrl+C.

--- bparchived.cpp
+++ bparchived.cpp
@@ -12,26 +12,41 @@
 #include "dectris_utils.h"
 #include "stream_to_cbf.h"
 
-// TODO: ZeroMQ doesn't respect our signal handler, and socket reads get
-//       interrupted by SIGINT (failing with EINTR).
-std::function<void()> shutdown_adapter_func; // I must always be reentrant!
-static void signal_handler(int signum) {
-  // iostream and printf are not signal-safe (reentrant), but plain-old write() is.
-  // So are all the functions called below.
+static void noop() {}
+
+std::atomic<int> signal_count=0;
+std::function<void()> signal_safe_shutdown_adapter_func = noop; // I must always be reentrant!
+static void signal_handler(int signum) {  
+  // iostream and printf() are not signal-safe (reentrant), but plain-old
+  // write() is, and so are all the functions called below.
   char strbuf[1024];
   memset(strbuf, '\0', sizeof(strbuf));
   strlcat(strbuf, "bparchived received the \"", sizeof(strbuf));
   strlcat(strbuf, strsignal(signum), sizeof(strbuf));
-  strlcat(strbuf, "\" signal. Shutdown will complete after any currently-running "
-	  "image series is completed.\n", sizeof(strbuf));
-  write(STDOUT_FILENO, strbuf, strlen(strbuf));
-  fsync(STDOUT_FILENO); // flush terminal output immediately
-
-  shutdown_adapter_func();
+  
+  if (++signal_count > 1) {
+    strlcat(strbuf, "\" signal for the second time. "
+	    "Forcing shutdown immediately.\n", sizeof(strbuf));
+    write(STDOUT_FILENO, strbuf, strlen(strbuf));
+    fsync(STDOUT_FILENO); // flush terminal output immediately
+    signal_safe_shutdown_adapter_func();
+    kill(getpid(), SIGKILL);
+    
+  } else {
+    strlcat(strbuf, "\" signal. Shutdown will complete after any "
+	    "currently-running image series is completed.\n", sizeof(strbuf));
+    write(STDOUT_FILENO, strbuf, strlen(strbuf));
+    fsync(STDOUT_FILENO); // flush terminal output immediately
+    signal_safe_shutdown_adapter_func();
+  }
 }
 
 static void usage() {
-  std::cerr << "bparchived -c <config_file>" << std::endl;
+  std::cerr << "bparchived [-c config_file]\n"
+	    << "  -c config_file : A JSON config file, see README for details.\n"
+	    << "\n"
+	    << "Receives images from an X-Ray detector and archives them to permanent storage.\n"
+	    << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -55,7 +70,7 @@ int main(int argc, char** argv) {
       return 1;
     }
   }
-
+  
   // Initialize IPC mechanisms with parent process.
   struct sigaction action;
   action.sa_handler = signal_handler;
@@ -63,11 +78,11 @@ int main(int argc, char** argv) {
   sigemptyset(&action.sa_mask);
   sigaction(SIGINT, &action, NULL);
   sigaction(SIGTERM, &action, NULL);
-
+  
   auto& config = load_config_file(config_file);
   stream_to_cbf parser(config);
   dectris_streamer<stream_to_cbf> streamer(std::ref(parser), config);
-  shutdown_adapter_func = [&]() { streamer.shutdown(); };
+  signal_safe_shutdown_adapter_func = [&]() { streamer.shutdown(); };
   streamer.run();
   std::clog << "INFO: done" << std::endl;
   
